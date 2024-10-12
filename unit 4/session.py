4730689cb83c656db3bd2dd1d5d3612ffbe07c82
warramma <wishmorethewitch@gmail.com>
2024-10-12 13:13:39 -0500

    unit 3, start unit 4

---
+++ unit 4/session.py
@@ -0,0 +1,32 @@
+def identify_popular_creators(nft_collection):
+    freq_dict = {}
+    for nft in nft_collection: 
+        artist_name = nft.get("creator")
+        if artist_name in freq_dict:
+            freq_dict[artist_name] += 1
+        else:
+            freq_dict[artist_name] = 1
+    
+    popular = [artist for artist, count in freq_dict.items() if count > 1]
+
+    return popular
+
+nft_collection = [
+    {"name": "Abstract Horizon", "creator": "ArtByAlex", "value": 5.4},
+    {"name": "Pixel Dreams", "creator": "DreamyPixel", "value": 7.2},
+    {"name": "Urban Jungle", "creator": "ArtByAlex", "value": 4.5}
+]
+
+nft_collection_2 = [
+    {"name": "Crypto Kitty", "creator": "CryptoPets", "value": 10.5},
+    {"name": "Galactic Voyage", "creator": "SpaceArt", "value": 6.7},
+    {"name": "Future Galaxy", "creator": "SpaceArt", "value": 8.3}
+]
+
+nft_collection_3 = [
+    {"name": "Golden Hour", "creator": "SunsetArtist", "value": 8.9}
+]
+
+print(identify_popular_creators(nft_collection))
+print(identify_popular_creators(nft_collection_2))
+print(identify_popular_creators(nft_collection_3))
