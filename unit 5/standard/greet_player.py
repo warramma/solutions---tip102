fa0fe167f29e2d7c60af5fb537b63a6a2ebe9db7
warramma <wishmorethewitch@gmail.com>
2024-10-22 14:22:04 -0500

    solve problem 5

--- unit 5/standard/greet_player.py
+++ unit 5/standard/greet_player.py
@@ -21,21 +21,38 @@ class Villager:
                
             print("catchphrase updated!")
             self.catchphrase = new_catchphrase
+    def add_item(self, item_name):
+        valid = ["acoustic guitar","ironwood kitchenette", "rattan armchair", "kotatsu", "cacao tree"]
+
+        if item_name in valid:
+            self.furniture.append(item_name)
     
 
-bones = Villager("Bones", "Dog", "yip yip")
-bones.catchphrase = "ruff it up"
+# bones = Villager("Bones", "Dog", "yip yip")
+# bones.catchphrase = "ruff it up"
 
-print(bones.greet_player("Wardiyah"))
-print(bones.name)
-print(bones.species)  
-print(bones.catchphrase) 
-print(bones.furniture) 
+# print(bones.greet_player("Wardiyah"))
+# print(bones.name)
+# print(bones.species)  
+# print(bones.catchphrase) 
+# print(bones.furniture) 
 
+
+# alice = Villager("Alice", "Koala", "guvnor")
+
+# alice.set_catchphrase("sweet dreams")
+# print(alice.catchphrase)
+# alice.set_catchphrase("#?!")
+# print(alice.catchphrase)
 
 alice = Villager("Alice", "Koala", "guvnor")
+print(alice.furniture)
 
-alice.set_catchphrase("sweet dreams")
-print(alice.catchphrase)
-alice.set_catchphrase("#?!")
-print(alice.catchphrase)
+alice.add_item("acoustic guitar")
+print(alice.furniture)
+
+alice.add_item("cacao tree")
+print(alice.furniture)
+
+alice.add_item("nintendo switch")
+print(alice.furniture)
