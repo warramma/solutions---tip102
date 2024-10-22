a5928a9dea20271b5c64aa6dc1c9b3d203abd25f
warramma <wishmorethewitch@gmail.com>
2024-10-22 14:19:50 -0500

    solve problem 4

--- unit 5/standard/greet_player.py
+++ unit 5/standard/greet_player.py
@@ -9,6 +9,20 @@ class Villager:
     def greet_player(self, player_name):
         return f"{self.name}: Hey there, {player_name}! How's it going, {self.catchphrase}!"
 
+    def set_catchphrase(self, new_catchphrase):
+        alphabet = "abcdefghijklmnopqrstuvwxyz "
+        if len(new_catchphrase) >= 20:
+            return "Invalid catchphrase"
+        else:
+            for char in new_catchphrase:
+                if char.lower() not in alphabet:
+                    print("Invalid catchphrase")
+                    return 
+               
+            print("catchphrase updated!")
+            self.catchphrase = new_catchphrase
+    
+
 bones = Villager("Bones", "Dog", "yip yip")
 bones.catchphrase = "ruff it up"
 
@@ -17,3 +31,11 @@ print(bones.name)
 print(bones.species)  
 print(bones.catchphrase) 
 print(bones.furniture) 
+
+
+alice = Villager("Alice", "Koala", "guvnor")
+
+alice.set_catchphrase("sweet dreams")
+print(alice.catchphrase)
+alice.set_catchphrase("#?!")
+print(alice.catchphrase)
